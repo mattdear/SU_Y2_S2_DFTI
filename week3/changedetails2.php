de82bb58eb3aeb0cae243cdeb85b449597f98327
Matthew Dear <0dearm11@solent.ac.uk>
2020-02-11 13:05:06 +0000

    week3 work is now complete.

--- week3/changedetails2.php
+++ week3/changedetails2.php
@@ -20,52 +20,19 @@
     $i = $_POST["downloads"];
     $j = $_POST["genre"];
     $k = $_POST["price"];
-    $q = $_POST["quantity"];
+    $l = $_POST["quantity"];
 
-    if($a == ""){
+    $result = $conn->query("SELECT * FROM wadsongs WHERE id = '$a'");
 
-      echo "You must enter an username.";
+    if($result->rowCount() == 0){
+
+      echo "ID incorrect and record has not been updated.";
 
     } else {
 
-        $results = $conn->query("SELECT * FROM wadsongs WHERE id = '$a'");
+      $conn->query("UPDATE wadsongs SET title = '$b', artist = '$c', day = '$d', month ='$e', year = '$f', chart = '$g', likes = '$h', downloads = '$i', genre = '$j', price = '$k', quantity = '$l' WHERE ID = '$a'");
 
-        if($results->rowCount() == 0) {
-
-          echo "Invalid username please check and try again.";
-
-        } else {
-
-          while($row=$results->fetch(PDO::FETCH_ASSOC)){
-          echo "<form method='post' action='changedetails2.php'>";
-          echo "<label>ID</label>";
-          echo "<input type='hidden' name='id' value=" . $row["ID"] . ">";
-          echo "<label>Title</label><br>";
-          echo "<input name='title' value=" . $row["title"] . "><br><br>";
-          echo "<label>Artist</label><br>";
-          echo "<input name='artist' value=" . $row["artist"] . "><br><br>";
-          echo "<label>Day</label><br>";
-          echo "<input name='day' value=" . $row["day"] . "><br><br>";
-          echo "<label>Month</label><br>";
-          echo "<input name='month' value=" . $row["month"] . "><br><br>";
-          echo "<label>Year</label><br>";
-          echo "<input name='year' value=" . $row["year"] . "><br><br>";
-          echo "<label>Chart</label><br>";
-          echo "<input name='chart' value=" . $row["chart"] . "><br><br>";
-          echo "<label>Likes</label><br>";
-          echo "<input name='likes' value=" . $row["likes"] . "><br><br>";
-          echo "<label>Downloads</label><br>";
-          echo "<input name='downloads' value=" . $row["downloads"] . "><br><br>";
-          echo "<label>Genre</label><br>";
-          echo "<input name='genre' value=" . $row["genre"] . "><br><br>";
-          echo "<label>Price</label><br>";
-          echo "<input name='price' value=" . $row["price"] . "><br><br>";
-          echo "<label>Quantity</label><br>";
-          echo "<input name='quantity' value=" . $row["quantity"] . "><br><br>";
-          echo "<input type='submit' value='Go!'>";
-          echo "</form>";
-        }
-      }
+      echo "Record $a updated.";
     }
   } catch(PDOException $e) {
       echo "Error: $e";
