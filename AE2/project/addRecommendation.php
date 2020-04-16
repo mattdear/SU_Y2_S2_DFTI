47feaa35eab78c0871887d6ab6d5af65e63151ec
Matt Dear <mrmdear@gmail.com>
2020-04-16 15:16:00 +0100

    I have now made it so add recommedation and add review are complete pages.

--- AE2/project/addRecommendation.php
+++ AE2/project/addRecommendation.php
@@ -1,7 +1,31 @@
 <?php
 include("functions.php");
 include("poiDAO.php");
-
+?>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="css/style.css">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+  <title>Points Of Interest - Reviews</title>
+</head>
+<body>
+  <div id="all_content">
+    <header>
+      <?php
+      title($_SESSION["isadmin"], $byDefault = 0);
+      if (isset ($_SESSION["gatekeeper"]))
+      {
+        echo "<p>Logged In User, " . $_SESSION["gatekeeper"] . "</p><br>";
+      }
+      backbutton();
+      ?>
+      <h2>Region Search</h2>
+    </header>
+    <?php
+    try{
 $id = $_POST["id"];
 $region = $_POST["region"];
 
@@ -12,5 +36,12 @@ if(preg_match("/^[0-9]$/", $id) || preg_match("/^[a-zA-Z0-9]{2,30}$/", $region))
 } else {
   echo "Somthing went wrong";
 }
-
+} catch(PDOException $e) {
+    echo "Error: $e";
+}
 ?>
+</div>
+<!--</div id="all_content"-->
+<?php footer()?>
+</body>
+</html>
--- AE2/project/approveReview.php
+++ AE2/project/approveReview.php
@@ -2,6 +2,31 @@
 session_start();
 include("functions.php");
 include("reviewsDAO.php");
+?>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="css/style.css">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+  <title>Points Of Interest - Reviews</title>
+</head>
+<body>
+  <div id="all_content">
+    <header>
+      <?php
+      title($_SESSION["isadmin"], $byDefault = 0);
+      if (isset ($_SESSION["gatekeeper"]))
+      {
+        echo "<p>Logged In User, " . $_SESSION["gatekeeper"] . "</p><br>";
+      }
+      backbutton();
+      ?>
+      <h2>Region Search</h2>
+    </header>
+    <?php
+    try{
 
 if ( !isset ($_SESSION["gatekeeper"]))
 {
@@ -18,6 +43,12 @@ if(preg_match("/^[0-9]$/", $id){
 } else {
   echo "Somthing went wrong";
 }
-
+} catch(PDOException $e) {
+    echo "Error: $e";
 }
 ?>
+</div>
+<!--</div id="all_content"-->
+<?php footer()?>
+</body>
+</html>
